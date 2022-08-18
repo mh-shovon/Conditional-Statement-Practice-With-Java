05152247ffe88d30227392b92ae96339d275ad96
MEHEDI HASAN SHOVON <shovon33266@gmail.com>
2022-08-18 21:04:52 +0600

    changes in prime number code

--- src/ForLoopPractice6.java
+++ src/ForLoopPractice6.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 public class ForLoopPractice6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int i,flag=0;
+        int i;
+        boolean flag = true;
+        System.out.println("Enter a number: ");
         int n = input.nextInt();
-        if(n==0||n==1){
-            System.out.println(n+" is not prime number");
-        }else{
             for(i=2;i<=n/2;i++){
-                if(n%i==0){
-                    System.out.println(n+" is not prime number");
-                    flag=1;
+                if(n%i == 0){
+                    flag = false;
                     break;
                 }
             }
-            if(flag==0)  { System.out.println(n+" is prime number"); }
-        }
+            if(flag == true){
+                System.out.println(n+" is prime number");
+            }
+            else{
+                System.out.println(n+" is non prime number");
+            }
 
 
         /*Scanner sc= new Scanner(System.in);
