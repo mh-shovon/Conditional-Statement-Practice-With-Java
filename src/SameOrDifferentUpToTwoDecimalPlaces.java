868b56963de2b58776ddc814b9b31c98527e6f0c
MEHEDI HASAN SHOVON <shovon33266@gmail.com>
2022-08-18 19:40:35 +0600

    check number are same or not up to two floating point

--- src/SameOrDifferentUpToTwoDecimalPlaces.java
+++ src/SameOrDifferentUpToTwoDecimalPlaces.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class SameOrDifferentUpToTwoDecimalPlaces {
+import java.util.Scanner;
+
+public class SameOrDifferentUpToTwoDecimalPlaces {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Number-1: ");
+        double num1 = input.nextDouble();
+        System.out.println("Enter Number-2: ");
+        double num2 = input.nextDouble();
+        if(Math.abs(num1-num2)<=0.001){
+            System.out.println("These numbers are same.");
+        }
+        else {
+            System.out.println("These numbers are not same");
+        }
+    }
 }
